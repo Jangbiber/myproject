645c865fc972cf9846251d616c646e070c3c615a
sinabiber <chocodufu@gmail.com>
2023-04-12 18:19:14 +0900

    커밋3 회원가입/로그인/로그아웃

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
@@ -9,3 +11,4 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email")
+        
